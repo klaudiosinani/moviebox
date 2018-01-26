e5ddc6fed5c39164fd2ce715608bed4bfc907e96
Klaus Sinani <klauscfhq@protonmail.com>
2018-01-26 06:11:32 +0200

    PyPi setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from os.path import join, dirname
+
+longDescription = open(join(dirname(__file__), 'readme.md')).read()
+
+setup(
+    name='moviebox',
+    version='0.0.0',
+    url='https://github.com/klauscfhq/moviebox',
+    license='MIT',
+    author='Klaus Sinani',
+    author_email='klauscfhq@protonmail.com',
+    description='Machine learning movie recommender',
+    long_description=longDescription,
+    include_package_data=True,
+    packages=['moviebox'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ],
+    install_requires=[
+        'click', 'pandas', 'scikit-learn', 'termcolor', 'colorama'
+    ],
+    entry_points={
+        'console_scripts': ['moviebox=moviebox.cli:main'],
+    })
