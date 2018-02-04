e3fe7d31a016816b4fe88daa9e2649cd7350aa84
Klaudio Sinani <klaudiosinani@protonmail.ch>
2018-02-04 03:45:39 +0200

    Support for Python 2.7+

--- fabfile.py
+++ fabfile.py
@@ -1,55 +1,39 @@
-import sys
-
-from fabric.colors import red
+# -*- coding: utf-8 -*-
 from fabric.api import local, task
-
-
-@task(default=True)
-def check():
-    # Get python version
-    PY3 = sys.version_info[0] == 3
-    # Python 3 check
-    if not (PY3):
-        print(red('Please install Python 3 to build the project.'))
-        quit()
 
 
 @task
 def clean():
     # Clean-up compiled files
-    local('./bin/clean.py && fclear python')
+    local('python ./bin/clean.py && fclear python')
 
 
 @task(alias='i')
 def install():
-    check()
     # Install dependencies
-    local('pip3 install -r requirements.txt')
+    local('pip install -r requirements.txt')
 
 
-@task
+@task(default=True)
 def test():
-    check()
     # Check code for errors
     local('flake8 --show-source --count')
 
 
 @task
 def fix():
-    check()
     # Automatically fix code errors with yapf
     local('yapf -ir .')
 
 
 @task
 def start():
-    check()
     # Run the API
-    local('python3 -m moviebox.main')
+    local('python -m moviebox.main')
 
 
 @task
 def dist():
-    check()
+
     # Build the source code
-    local('python3 setup.py sdist bdist_wheel')
+    local('python setup.py sdist bdist_wheel')
